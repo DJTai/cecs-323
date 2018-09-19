0b41bdf557abe2f51635fca3938bf493ca37160c
David T <djtai@protonmail.com>
2018-09-19 09:28:42 -0700

    Add product and OrderLine tables

--- labs/lab03/lab03a.sql
+++ labs/lab03/lab03a.sql
@@ -19,3 +19,27 @@ CREATE TABLE "Order" (
         REFERENCES customer(customer_id),
     CONSTRAINT  orders_pk PRIMARY KEY (customer_id, orderdate)
 );
+
+CREATE TABLE product (
+    upc         INTEGER NOT NULL PRIMARY KEY,
+    prodName    VARCHAR(40) NOT NULL,
+    mfgr        VARCHAR(40) NOT NULL,
+    model       VARCHAR(20) NOT NULL,
+    unitListPrice   DOUBLE NOT NULL,
+    unitsInStock    INTEGER NOT NULL
+);
+
+ALTER TABLE product ADD CONSTRAINT product_uk01 UNIQUE(prodName, mfgr, model);
+
+CREATE TABLE OrderLine (
+    customer_id             INTEGER NOT NULL,
+    orderDate               DATE NOT NULL,
+    UniversalProductCode    INTEGER NOT NULL,
+    quantity                INTEGER NOT NULL,
+    unitSalePrice   DOUBLE,
+    CONSTRAINT orderline_pk PRIMARY KEY(customer_id, orderDate, UniversalProductCode),
+    CONSTRAINT OrderLine_Order_fk01 FOREIGN KEY(customer_id, orderDate)
+        REFERENCES "Order"(customer_id, orderdate),
+    CONSTRAINT OrderLine_product FOREIGN KEY(UniversalProductCode)
+        REFERENCES product(upc)
+);
