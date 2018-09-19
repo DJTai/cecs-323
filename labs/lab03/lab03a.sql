dc8fa47e44a1761c763e9334335b58b5174869a3
David T <djtai@protonmail.com>
2018-09-19 09:44:47 -0700

    Insert statements for customer and product tables

--- labs/lab03/lab03a.sql
+++ labs/lab03/lab03a.sql
@@ -43,3 +43,17 @@ CREATE TABLE OrderLine (
     CONSTRAINT OrderLine_product FOREIGN KEY(UniversalProductCode)
         REFERENCES product(upc)
 );
+
+INSERT INTO customer(cFirstName, cLastName, cPhone, cStreet, cZipcode)
+VALUES ('Dave', 'Brown', '562-982-8696', '123 Lakewood Blvd. Long Beach', '90080'),
+    ('Rachel', 'Burris', '213-543-2211', '54 218th St. Torrance', '90210'),
+    ('Tom', 'Jewett', '714-555-1212', '10200 Slater', '92708'),
+    ('Alvero', 'Monge', '562-111-1234', '314159 Pi St. Long Beach', '90814');
+
+INSERT INTO product(UPC, prodName, mfgr, model, unitListPrice, unitsInStock)
+VALUES (135798642, 'Framing hammer, 20oz.', 'Stanley', 'Frame01', 18.95, 20),
+(123456789, 'Framer''s level 9 ft.', 'Stanley', 'Frame09', 28.57, 10),
+(777999111, 'Blade Screwdriver #2', 'Proto', 'Blad02', 8.53, 15),
+(123123123, 'Cold Chisel 1"', 'Challenger', 'One inch', 12.04, 30),
+(321321321, 'Jackhammer, Electric', 'Bosche', 'Sml Elec', 128.95, 5),
+(111222333, 'Arc Welder', 'Lincoln', 'Industrial', 5298.65, 1);
